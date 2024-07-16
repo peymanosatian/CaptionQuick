b37dca3d7ab28b37934f9c0e9b7005a554b8347f
Peyman <peyman.osatian@gmail.com>
2024-07-16 21:54:35 +0200

    Initial Commit

---
+++ CaptionQuick/CaptionQuickApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptionQuickApp.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-07-16.
+//
+
+import SwiftUI
+
+@main
+struct CaptionQuickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptionQuick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
